cd33544438a3e63c2a05b89a692d9489d4cadc17
jahnavi-celestial <jahnavi.gaur@celestialsys.com>
2026-05-24 11:56:03 +0530

    change cors

--- backend/app.ts
+++ backend/app.ts
@@ -16,7 +16,8 @@ app.use(cors({
   origin: process.env.FRONTEND_URL,
   methods: 'GET,POST,PUT,DELETE,OPTIONS',
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
+  credentials: true,
+  optionsSuccessStatus: 200
 }))
 
 app.use(express.json());
